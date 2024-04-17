5ba779b59f288ea660f90f4b09ae25ec70d9ef74
Honi <1095175021@qq.com>
2024-04-17 23:52:21 +0800

    feat: 创建基本对象

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
+class Count extends React.Component {
+  state = { count: 0 }
+  render() {
+    const { count } = this.state
+    return (
+      <>
+        <div>{count}</div>
+        <button
+          onClick={() => {
+            this.setState({
+              count: count + 1,
+            })
+          }}
+        >
+          +1
+        </button>
+      </>
+    )
+  }
+}
+
+root.render(<Count />)
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => response.json())
+//   .then(json => console.log(json))
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
